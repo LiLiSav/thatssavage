147fb4f7be97e727275377bb372ca6ce04264188
LiLiSav <liamsav@hotmail.com>
2021-01-04 20:12:34 +0000

    imported images to item cards

--- src/components/ItemCards.js
+++ src/components/ItemCards.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import './CardOptions.css';
 import CardOptions from './CardOptions';
+import masks from "./images/mask.jpg";
+import business from "./images/business.jpg";
+import hens from "./images/hens.jpg";
+import sports from "./images/sporting.jpg";
+import lappin from "./images/lappin.jpg";
+import school from "./images/school.jpg";
+import jersey from "./images/jersey.jpg";
+import misc from "./images/misc.jpg";
+import occasion from "./images/occasion.jpg";
 
-/*text1 = "Business Logos"
-text2 = "Sporting Teams"
-text3 = "Professional Settings"
-text4 = "Small, Medium & Large fit"
-
-objects={"Business Logos", "Sporting Teams", "Professional Settings", "Small, Medium & Large fit"}*/
 
 export default function ItemCards() {
     return (
@@ -18,21 +21,21 @@ export default function ItemCards() {
             
             <ul className="cards-container">
                 <CardOptions
-                    src="./images/mask.jpg"
+                    src={masks}
                     listText={["Business Logos", "Sporting Teams", "Professional Settings", "Small, Medium & Large fit"]}
                     label="Personalised Face Masks"
                     alt="Customer ready masks"
                     bgColour= "dgry"   
                 />
                 < CardOptions
-                    src="./images/business.jpg"
+                    src={business}
                     listText={["Polo Shirts/T-Shirts", "Softshell Jackets", "Hoodies/Sweatshirts", "High Visibility Workwear, etc."]}
                     label="Business Printing"
                     alt="Busines ready attire"
                     bgColour="yllw"
                 />
                 <CardOptions
-                    src="./images/hens.jpg"
+                    src={hens}
                     listText={["Custom Design", "Hen/Stag Print Free"]}
                     label="Hen & Stag Parties"
                     colour="#000"
@@ -41,21 +44,21 @@ export default function ItemCards() {
                 />
 
                 <CardOptions
-                    src="./images/sporting.jpg"
+                    src={sports}
                     listText={["GAA Club Teams", "GAA County Teams", "Rugby Teams", "Football Teams, etc."]}
                     label="Sporting Attire"
                     alt="Sporting Garments printed"
                     bgColour="lgry"
                 />
                 <CardOptions
-                    src="./images/lappin.jpg"
+                    src={lappin}
                     listText={["Simple, Clean, Effective Design", "PDF version for stationary print & PNG for online & Social Media"]}
                     label="Graphic Design"
                     alt="Lappin Logo"
                     bgColour="dgry"
                 />
                 <CardOptions
-                    src="./images/school.jpg"
+                    src={school}
                     listText={["Leaver Hoodies", "School Gym Sacs", "School Gear", "School T-Shirts, etc."]}
                     label="Graduation Printing"
                     alt="Personalised Graduation Garments"
@@ -63,7 +66,7 @@ export default function ItemCards() {
                 />
 
                 <CardOptions
-                    src="./images/jersey.jpg"
+                    src={jersey}
                     listText={["Name & Number Print", "Premier League, SPL, Seria A, La Liga, etc." ]}
                     label="Replica Jersey Printing"
                     alt="Personilised replica jerseys"
@@ -71,14 +74,14 @@ export default function ItemCards() {
 
                 />
                 <CardOptions
-                    src="./images/misc.jpg"
+                    src={misc}
                     listText={["Hip Flasks / Umbrellas", "Mouse Mats / Teddy Bears", "Water Bottle Sleeve", "High Vis/Glow in the Dark Print"]}
                     label="Other Printing Options"
                     alt="Misc. printing options"
                     bgColour="dgry"
                 />
                 <CardOptions
-                    src="./images/occasion.jpg"
+                    src={occasion}
                     listText={["Confirmations / Communions", "Birthdays / Mothers/Fathers Day", "Seasonal Printing", "(St. Patrick's Day, Easter, Halloween, Christmas, etc.)"]}
                     label="Special Occasions"
                     alt="Memorable moments printing"
